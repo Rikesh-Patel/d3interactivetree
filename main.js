27a9a00892c2d73e54cf859cba0f09e733b3796b
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2023-05-16 12:32:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ let g = svg.append('g')
             .attr('transform', 'translate(140,50)');
 
     
-    let layout = d3.tree().nodeSize([15, 30]);
+    let layout = d3.tree().nodeSize([15, 100]);
     //.separation(function(a, b) {
     //return a.parent == b.parent ? 25 : 20;
 //});
